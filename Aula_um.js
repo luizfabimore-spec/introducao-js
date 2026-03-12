f3ffdae1f7117b8036a4874113dc52ceefbdac0b
Kayo <luiz.fabimore@gmail.com>
2026-03-12 09:06:49 -0300

    Update Aula_um.js

--- Aula_um.js
+++ Aula_um.js
@@ -2,4 +2,5 @@ const input = require('prompt-sync')();
 
 let nome = input("Digite seu nome: ");
 let sobrenome = input("Digite seu sobrenome: ");
-console.log("Nome completo: " + nome + " " + sobrenome);
+
+console.log(`Nome completo: ${nome} ${sobrenome}`);
